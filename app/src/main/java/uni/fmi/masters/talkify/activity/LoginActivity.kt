197cac1b9d107a44d67983b116823351b6c8bc06
Petar Ivaylov Ivanov <petar.ivanov@proxiad.com>
2025-04-27 13:07:02 +0300

    Implemented login activity and its logic

--- app/src/main/java/uni/fmi/masters/talkify/activity/LoginActivity.kt
+++ app/src/main/java/uni/fmi/masters/talkify/activity/LoginActivity.kt
@@ -1,12 +1,46 @@
 package uni.fmi.masters.talkify.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import uni.fmi.masters.talkify.R
+import uni.fmi.masters.talkify.service.api.UserApi
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity(private val userApi: UserApi) : AppCompatActivity() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + Job())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<Button>(R.id.login_btn).setOnClickListener {
+            onLogin()
+        }
+    }
+
+    private fun onLogin() {
+        val username = findViewById<TextInputLayout>(R.id.username_input).editText?.text.toString()
+        val password = findViewById<TextInputLayout>(R.id.password_input).editText?.text.toString()
+
+        if (username.isBlank() || password.isBlank()) {
+            findViewById<TextView>(R.id.login_error).text = getString(R.string.login_required_error)
+            return
+        }
+
+        try {
+            coroutineScope.launch {
+                userApi.login(username, password)
+            }
+            startActivity(Intent(this, TalkifyActivity::class.java))
+        } catch (e: Exception) {
+            findViewById<TextView>(R.id.login_error).text = getString(R.string.login_non_existing_error)
+        }
     }
 }
